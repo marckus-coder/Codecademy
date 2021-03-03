32ba26b53a869c9ba284eab3b17f82d2aa57a379
Marcus de Oliveira <70022309+marckus-coder@users.noreply.github.com>
2021-03-03 10:24:42 -0300

    Arrays Study

---
+++ Arrays/BuildingArrays.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace BuildingArrays
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     string[] summerStrut;  
+     summerStrut = new string[] {"Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Feel", "Rebel Rebel", "Despacito", "Los Angeles"}; 
+
+     int[] ratings = {1, 2, 3, 4, 5}; 
+      
+    }
+  }
+}
